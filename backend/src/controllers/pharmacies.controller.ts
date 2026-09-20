55e930e1a10bff208a58c4c6888fea7ecd6a146b
Val Pedro <valpedro59@gmail.com>
2026-09-20 17:05:28 +0100

    fix: add .js extension to db import in pharmacies controller

--- backend/src/controllers/pharmacies.controller.ts
+++ backend/src/controllers/pharmacies.controller.ts
@@ -1,4 +1,4 @@
-import { db } from "../config/db";
+import { db } from "../config/db.js";
 import { eq } from "drizzle-orm";
 import { arrondissements, pharmacies, villes } from "../models/schema.js";
 
