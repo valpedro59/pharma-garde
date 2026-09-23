d1bb66e4da5c4e5f73290fa656de2eb8a5ed6695
Val Pedro <valpedro59@gmail.com>
2026-09-23 18:54:59 +0100

    fix: image backgroung tailwind

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <RechercheProvider>
       <div className="relative">
-        <div className="relative w-full bg-[url('bg.webp')] bg-cover bg-center overflow-hidden">
+        <div className="relative w-full bg-[url('/bg.webp')] bg-cover bg-center overflow-hidden">
           <div className="absolute inset-0 bg-linear-to-r from-emerald-100/90 via-slate-50/40 to-transparent  pointer-events-none" />
           <div className="absolute -top-20 -left-20 w-80 h-80 bg-emerald-200/40 rounded-full blur-3xl pointer-events-none" />
           <div className="relative z-10">
